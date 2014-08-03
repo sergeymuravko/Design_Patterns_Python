e1f5ce7f91c109778e818dde5c332aca959598d1
sergey <sergeymuravko@gmail.com>
2014-08-03 18:28:58 +0300

    comment #1

--- strategy.py
+++ strategy.py
@@ -1,1 +1,17 @@
 __author__ = 'sergey'
+
+
+class AbstractDuck:
+    def __init__(self, name):
+        self.name = name
+
+    def swim(self):
+        print "I'm swimming"
+
+
+class Flying:
+    def fly(self):
+        print "I'm fly"
+
+
+
