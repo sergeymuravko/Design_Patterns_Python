207b8e8b3aab830eb2fe93bd2e5f4190c1f6551b
sergey <sergeymuravko@gmail.com>
2014-08-07 15:07:58 +0300

    comment #1

--- factory.py
+++ factory.py
@@ -1,1 +1,34 @@
 __author__ = 'sergey'
+
+
+class PizzaFactory:
+    def __init__(self, pizzaType):
+        print "I'm pizza Factory"
+        self.pizzaType = pizzaType
+
+    def createPizza(self):
+        targetPizza = self.pizzaType#.capitalize()
+        #return globals()[targetPizza]()
+        self.pizza = globals()[targetPizza]()
+        print self.pizza
+
+
+class AbstractPizza:
+    pizza = ""
+
+    def get_pizza(self):
+
+        return self.pizza
+
+
+class SanFranciscoPizza(AbstractPizza):
+    pizza = 'thin crust, lots of cheese, sauce, ham'
+
+
+class ChicagoPizza(AbstractPizza):
+    pizza = 'thin dough, a little cheese, gravy, sausage'
+
+
+if __name__ == '__main__':
+    pizza1 = PizzaFactory('ChicagoPizza')
+    print pizza1.createPizza()
